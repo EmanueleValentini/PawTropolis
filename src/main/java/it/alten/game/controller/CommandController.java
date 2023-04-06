456fce261cf85d0fcd60017ca4de3cd81e19a803
EmanueleValentini <emanuele_valentini@hotmail.it>
2023-04-06 11:56:11 +0200

    Implementation of commandProcessing

--- src/main/java/it/alten/game/controller/CommandController.java
+++ src/main/java/it/alten/game/controller/CommandController.java
@@ -1,8 +1,14 @@
 package it.alten.game.controller;
 
+import it.alten.game.model.enums.AdjacentRooms;
+
 public class CommandController {
 
     private String command;
+
+    private RoomController roomController;
+
+    private GameController gameController;
 
     public CommandController() {
 
@@ -14,5 +20,32 @@ public class CommandController {
 
     public void setCommand(String command) {
         this.command = command;
+    }
+
+    public void commandProcessing(String command) {
+        if (command.equalsIgnoreCase("go north") &&
+                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.NORTH))){
+                gameController.changeRoom(AdjacentRooms.NORTH);
+                System.out.println(roomController.getCurrentRoom().roomDescription());
+
+        }
+        if (command.equalsIgnoreCase("go south") &&
+                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.SOUTH))){
+                gameController.changeRoom(AdjacentRooms.SOUTH);
+                System.out.println(roomController.getCurrentRoom().roomDescription());
+
+        }
+        if (command.equalsIgnoreCase("go east") &&
+                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.EAST))){
+                gameController.changeRoom(AdjacentRooms.EAST);
+                System.out.println(roomController.getCurrentRoom().roomDescription());
+
+        }
+        if (command.equalsIgnoreCase("go west") &&
+                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.WEST))){
+                gameController.changeRoom(AdjacentRooms.WEST);
+                System.out.println(roomController.getCurrentRoom().roomDescription());
+
+        }
     }
 }
