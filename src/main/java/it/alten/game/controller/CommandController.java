b7ebecedd941a7c797b4aa311fab90b8db20d0b5
EmanueleValentini <emanuele_valentini@hotmail.it>
2023-04-06 15:39:40 +0200

    Command Controller Fixing

--- src/main/java/it/alten/game/controller/CommandController.java
+++ src/main/java/it/alten/game/controller/CommandController.java
@@ -1,14 +1,19 @@
 package it.alten.game.controller;
 
+import it.alten.game.model.Bag;
+import it.alten.game.model.Item;
+import it.alten.game.model.Player;
 import it.alten.game.model.enums.AdjacentRooms;
+
+import java.util.function.Function;
 
 public class CommandController {
 
     private String command;
 
-    private RoomController roomController;
+    private final RoomController roomController;
 
-    private GameController gameController;
+    private final GameController gameController;
 
     public CommandController() {
         this.roomController = new RoomController();
@@ -23,29 +28,58 @@ public class CommandController {
         this.command = command;
     }
 
-    public void commandProcessing(String command) {
-        if (command.equalsIgnoreCase("go north") &&
-                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.NORTH))){
-            gameController.changeRoom(AdjacentRooms.NORTH);
-            System.out.println(roomController.getCurrentRoom().roomDescription() + "\n");
+    public void look() {
+        System.out.println(roomController.getCurrentRoom().roomDescription() + "\n");
+    }
 
-        } else if (command.equalsIgnoreCase("go south") &&
-                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.SOUTH))){
-            gameController.changeRoom(AdjacentRooms.SOUTH);
-            System.out.println(roomController.getCurrentRoom().roomDescription() + "\n");
-
-        } else if (command.equalsIgnoreCase("go east") &&
-                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.EAST))){
-            gameController.changeRoom(AdjacentRooms.EAST);
-            System.out.println(roomController.getCurrentRoom().roomDescription() + "\n");
-
-        } else if (command.equalsIgnoreCase("go west") &&
-                (roomController.getCurrentRoom().getAdjacentRoomsList().containsKey(AdjacentRooms.WEST))){
-            gameController.changeRoom(AdjacentRooms.WEST);
-            System.out.println(roomController.getCurrentRoom().roomDescription() + "\n");
+    public void dropItem(Item item) {
+        if (gameController.getPlayer().getAllItemsInBag().contains(item)) {
+            gameController.getPlayer().removeItemFromBag(item);
+            roomController.getCurrentRoom().addItemToRoom(item);
         } else {
-            gameController.changeRoom(AdjacentRooms.NOT_VALID);
-            System.out.println("Non c'è nessuna porta lì coglione!" + "\n");
+            System.out.println("Sei n'cojone nun ce l'hai!");
+        }
+    }
+
+    public void commandProcessing(String command) {
+        if (command.equalsIgnoreCase("go north")) {
+            if (gameController.changeRoom(AdjacentRooms.NORTH) == null) {
+                System.out.println("Coglione non c'è una porta su quel lato");
+            } else {
+                gameController.changeRoom(AdjacentRooms.NORTH);
+                look();
+            }
+
+        } else if (command.equalsIgnoreCase("go south")) {
+            if (gameController.changeRoom(AdjacentRooms.SOUTH) == null) {
+                System.out.println("Coglione non c'è una porta su quel lato");
+            } else {
+                gameController.changeRoom(AdjacentRooms.SOUTH);
+                look();
+            }
+
+        } else if (command.equalsIgnoreCase("go east")) {
+            if (gameController.changeRoom(AdjacentRooms.EAST) == null) {
+                System.out.println("Coglione non c'è una porta su quel lato");
+            } else {
+                gameController.changeRoom(AdjacentRooms.EAST);
+                look();
+            }
+
+        } else if (command.equalsIgnoreCase("go west")) {
+            if (gameController.changeRoom(AdjacentRooms.WEST) == null) {
+                System.out.println("Coglione non c'è una porta su quel lato");
+            } else {
+                gameController.changeRoom(AdjacentRooms.WEST);
+                look();
+            }
+
+        } else if (command.equalsIgnoreCase("look")) {
+            look();
+        } else if (command.equalsIgnoreCase("bag")) {
+            System.out.println(gameController.getPlayer().getAllItemsInBag());
+        } else if (command.equalsIgnoreCase("drop " + roomController.getCurrentRoom().getRoomItemList()) {
+
         }
     }
 }
