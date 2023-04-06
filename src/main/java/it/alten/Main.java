e2bee5880a4fe4361b1d6e9d3fde18c616ce2d7f
EmanueleValentini <emanuele_valentini@hotmail.it>
2023-04-06 12:51:26 +0200

    Go methods Testing

--- src/main/java/it/alten/Main.java
+++ src/main/java/it/alten/Main.java
@@ -1,30 +1,17 @@
 package it.alten;
 
-import it.alten.animal.controller.ZooController;
-import it.alten.animal.model.*;
+import it.alten.game.controller.CommandController;
+import it.alten.game.controller.GameController;
 import it.alten.game.controller.RoomController;
-import it.alten.game.model.Bag;
-import it.alten.game.model.Item;
-import it.alten.game.model.Player;
-import it.alten.game.model.Room;
 
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        testZoo();
-    }
-    private static void testZoo() {
 
-		RoomController roomController = new RoomController();
-
-
-
+        CommandController commandController = new CommandController();
+        RoomController roomController = new RoomController();
+        System.out.println(roomController.getCurrentRoom().roomDescription());
+        commandController.commandProcessing("GO EaSt");
 
 
     }
