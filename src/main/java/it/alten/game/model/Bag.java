7be9bea23e3c6b45b4ee2f46aa86645999f55abc
EmanueleValentini <emanuele_valentini@hotmail.it>
2023-04-06 15:39:23 +0200

    Bag override Equals and Hashcode

--- src/main/java/it/alten/game/model/Bag.java
+++ src/main/java/it/alten/game/model/Bag.java
@@ -2,6 +2,7 @@ package it.alten.game.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Bag {
     private List<Item> inventory;
@@ -67,5 +68,18 @@ public class Bag {
                 "inventory=" + inventory +
                 ", availableSlots=" + availableSlots +
                 ", maxSlots=" + maxSlots;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bag bag = (Bag) o;
+        return availableSlots == bag.availableSlots && maxSlots == bag.maxSlots && Objects.equals(inventory, bag.inventory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inventory, availableSlots, maxSlots);
     }
 }
