5acec2e0890c1049d5fe52d10cdd0ced882b3463
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-18 11:59:42 +0200

    Fix main to make it work

--- src/main/java/it/alten/Main.java
+++ src/main/java/it/alten/Main.java
@@ -7,7 +7,7 @@ import it.alten.game.controller.GameController;
 public class Main {
     public static void main(String[] args) {
 
-        GameController gameController = new GameController();
+        GameController gameController = GameController.getInstance();
         gameController.runGame();
 
 
