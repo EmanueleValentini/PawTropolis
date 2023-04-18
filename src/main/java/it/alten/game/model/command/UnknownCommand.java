f3fc9081e877bdaa6243ceac00d888174bdf0562
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-18 14:05:49 +0200

    UnknownCommand implementation

--- src/main/java/it/alten/game/model/command/UnknownCommand.java
+++ src/main/java/it/alten/game/model/command/UnknownCommand.java
@@ -3,7 +3,7 @@ package it.alten.game.model.command;
 public class UnknownCommand extends Command {
 
     public void execute(String command){
-
+        System.out.println("Nun ho capito!");
     }
 
 }
