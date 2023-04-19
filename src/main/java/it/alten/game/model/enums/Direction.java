96627df973d8c8a4354cc9c8c9218e38b7d578e1
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-19 10:30:05 +0200

    create static method getOppositeDirection

--- src/main/java/it/alten/game/model/enums/Direction.java
+++ src/main/java/it/alten/game/model/enums/Direction.java
@@ -11,11 +11,26 @@ public enum Direction {
     private final String name;
 
     Direction(String name) {
-
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public static Direction getOppositeDirection(Direction direction) {
+        switch (direction){
+            case NORTH -> {
+                return SOUTH;
+            } case SOUTH -> {
+                return NORTH;
+            } case EAST -> {
+                return WEST;
+            } case WEST -> {
+                return EAST;
+            } default -> {
+                return NOT_VALID;
+            }
+        }
     }
 }
