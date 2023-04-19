0ab2d4fa73b4d1dbf74a97579eb56267f11039d9
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-19 11:01:56 +0200

    Refactoring Main, Remove CommandController (is useless now ;) )

--- src/main/java/it/alten/Main.java
+++ src/main/java/it/alten/Main.java
@@ -7,9 +7,7 @@ import it.alten.game.controller.GameController;
 public class Main {
     public static void main(String[] args) {
 
-        GameController gameController = GameController.getInstance();
-        gameController.runGame();
-
+        GameController.getInstance().runGame();
 
     }
 }
--- src/main/java/it/alten/game/model/command/GoCommand.java
+++ src/main/java/it/alten/game/model/command/GoCommand.java
@@ -1,6 +1,5 @@
 package it.alten.game.model.command;
 
-import it.alten.game.controller.GameController;
 import it.alten.game.model.enums.Direction;
 
 import static it.alten.game.model.CommandFactory.getParameter;
