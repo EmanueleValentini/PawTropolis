2ba78a03791aeab78d2600ffa8e95d4367f0f810
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-20 11:06:18 +0200

    CommandFactory is now stateless

--- src/main/java/it/alten/game/model/CommandFactory.java
+++ src/main/java/it/alten/game/model/CommandFactory.java
@@ -4,37 +4,21 @@ import it.alten.game.model.command.*;
 
 public class CommandFactory {
 
-    private static String parameter;
-
-    public static String getParameter() {
-        return parameter;
-    }
-
-    public static void setParameter(String parameter) {
-        CommandFactory.parameter = parameter;
-    }
-
     public Command createCommandFromString(String input) {
+        input = input.toLowerCase();
         if (input.startsWith("go")) {
-            setParameter(input);
-            return new GoCommand();
+            return new GoCommand(input);
         } else if (input.startsWith("get")) {
-            setParameter(input);
-            return new GetCommand();
+            return new GetCommand(input);
         } else if (input.startsWith("drop")) {
-            setParameter(input);
-            return new DropCommand();
-        } else if (input.equalsIgnoreCase("look")) {
-            setParameter(input);
+            return new DropCommand(input);
+        } else if (input.startsWith("look")) {
             return new LookCommand();
-        } else if (input.equalsIgnoreCase("bag")) {
-            setParameter(input);
+        } else if (input.startsWith("bag")) {
             return new BagCommand();
-        } else if (input.equalsIgnoreCase("quit")) {
-            setParameter(input);
+        } else if (input.startsWith("quit")) {
             return new QuitCommand();
         } else {
-            setParameter(input);
             return new UnknownCommand();
         }
     }
