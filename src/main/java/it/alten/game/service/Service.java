6163f4b16b1b784c06cab77582e36ab1b59942b6
Tasnim Chiara Uddin <chiaratasnim@live.it>
2023-04-24 17:27:22 +0200

    Update Service.java

--- src/main/java/it/alten/game/service/Service.java
+++ src/main/java/it/alten/game/service/Service.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class Service {
     @Bean
     public GameController gameController() {
-        return new GameController();
+        return new GameController(roomController());
     }
 
     @Bean
