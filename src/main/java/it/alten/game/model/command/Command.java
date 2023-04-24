09f4334267bf4b450fc5898e0fb908a4503af15c
Tasnim Chiara Uddin <chiaratasnim@live.it>
2023-04-24 17:32:45 +0200

    Update Command.java

--- src/main/java/it/alten/game/model/command/Command.java
+++ src/main/java/it/alten/game/model/command/Command.java
@@ -2,13 +2,16 @@ package it.alten.game.model.command;
 
 import it.alten.game.controller.GameController;
 import it.alten.game.controller.RoomController;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Getter
 @Component
+@NoArgsConstructor
 public abstract class Command {
 
     private RoomController roomController;
