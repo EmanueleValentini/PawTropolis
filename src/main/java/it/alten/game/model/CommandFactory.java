692fa02db38dd6e981d315e770b957ec108a4bcb
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-04-24 11:23:43 +0200

    Refactor

--- src/main/java/it/alten/game/model/CommandFactory.java
+++ src/main/java/it/alten/game/model/CommandFactory.java
@@ -1,10 +1,9 @@
 package it.alten.game.model;
 
 import it.alten.game.model.command.*;
-import it.alten.utils.CommandListMaker;
+import it.alten.utils.CommandSetMaker;
 import it.alten.utils.CommandTriggerMaker;
 
-import java.security.Key;
 import java.util.*;
 
 public class CommandFactory {
@@ -23,13 +22,13 @@ public class CommandFactory {
     }
 
     public Map<String,Class<? extends Command>> init () {
-        CommandListMaker commandListMaker = CommandListMaker.getInstance();
-        CommandTriggerMaker commandTriggerMaker = CommandTriggerMaker.getInstance();
-        List<Class<? extends Command>> commandClasses = new ArrayList<>(commandListMaker.getCommandList());
-        List<String> commandTrigger = new ArrayList<>(commandTriggerMaker.getCommandTriggerList());
+        Set<String> commandTrigger = CommandTriggerMaker.getInstance().getCommandTriggerSet();
+        Set<Class<? extends Command>> commandSet = CommandSetMaker.getInstance().getCommandSet();
+        List<String> commandTriggerList = new ArrayList<>(commandTrigger);
+        List<Class <? extends Command>> commandList = new ArrayList<>(commandSet);
         Map<String,Class<? extends Command>> commandPairs = new HashMap<>();
-        for (int i = 0; i <= commandTrigger.size(); i++) {
-            commandPairs.put(commandTrigger.get(i), commandClasses.get(i));
+        for (int i = 0; i < commandSet.size(); i++) {
+            commandPairs.put(commandTriggerList.get(i),commandList.get(i));
         }
         return commandPairs;
     }
@@ -38,7 +37,7 @@ public class CommandFactory {
     //TODO: Risolvere createCommandFromString, come faccio a recuperarmi la prima chiave della mappa?
     public Command createCommandFromString(String input) {
         Map<String,List<String>> commandData = inputSplitter(input);
-        Command command = commands.get(commandData.keySet());
+        
     }
 
     private Map<String, List<String>> inputSplitter(String input) {
