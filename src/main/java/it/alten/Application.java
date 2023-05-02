b0d647df4b71557c481f79ca15e179c89e24de11
Croissanto <antoniopapaleo24@gmail.com>
2023-05-02 10:54:02 +0200

    Update Application.java

--- src/main/java/it/alten/Application.java
+++ src/main/java/it/alten/Application.java
@@ -1,15 +1,13 @@
 package it.alten;
 
 import it.alten.game.controller.GameController;
-import it.alten.game.controller.RoomController;
-import it.alten.game.model.CommandFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = GameController.getInstance();
-        gameController.runGame();
+        GameController.getInstance().runGame();
+
     }
 }
