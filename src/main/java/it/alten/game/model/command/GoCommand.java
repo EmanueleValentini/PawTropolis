eeac08ab2ddd4643651c9f6ec53c7a6272827a4c
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-05-02 09:49:51 +0200

    GoCommand fix import

--- src/main/java/it/alten/game/model/command/GoCommand.java
+++ src/main/java/it/alten/game/model/command/GoCommand.java
@@ -1,9 +1,12 @@
 package it.alten.game.model.command;
 
 import it.alten.game.controller.GameController;
+import it.alten.game.model.Room;
 import it.alten.game.model.enums.Direction;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Map;
 
 @Getter
 @Setter
