54c09d961512b433ff7424dcf7bfb1d657c69d23
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-05-03 10:43:12 +0200

    Fix spacing

--- src/main/java/it/alten/Application.java
+++ src/main/java/it/alten/Application.java
@@ -8,6 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         GameController.getInstance().runGame();
-
     }
 }
