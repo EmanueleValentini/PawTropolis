f2c54434ab5d7c02e1ab304e7dac21ea1f9d89c7
Tasnim Chiara Uddin <chiaratasnim@live.it>
2023-05-08 09:32:38 +0200

    Update Application.java

--- src/main/java/it/alten/Application.java
+++ src/main/java/it/alten/Application.java
@@ -1,12 +1,14 @@
 package it.alten;
 
 import it.alten.game.controller.GameController;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
         GameController.getInstance().runGame();
     }
 }
