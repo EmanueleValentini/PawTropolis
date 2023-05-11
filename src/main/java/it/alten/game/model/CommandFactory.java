24eb1b07d85f5dced7fcb641e473cd98ac7a895a
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-05-11 12:44:41 +0200

    CommandFactory first fix

--- src/main/java/it/alten/game/model/CommandFactory.java
+++ src/main/java/it/alten/game/model/CommandFactory.java
@@ -1,6 +1,9 @@
 package it.alten.game.model;
 
 import it.alten.game.model.command.*;
+import it.alten.game.utils.CommandSetMaker;
+import it.alten.game.utils.CommandTriggerMaker;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -12,24 +15,37 @@ import java.util.Map;
 
 @Component
 @NoArgsConstructor
+@Data
 public class CommandFactory {
 
 
     protected Map<String, List<String>> commands;
+
     private ApplicationContext context;
 
+    private CommandSetMaker commandSet;
+
+    private CommandTriggerMaker commandKeyWords;
+
     @Autowired
-    private CommandFactory(ApplicationContext context) {
+    private CommandFactory(ApplicationContext context, CommandSetMaker commandSet, CommandTriggerMaker commandKeyWords) {
         this.context = context;
         this.commands = new HashMap<>();
+        this.commandKeyWords = commandKeyWords;
+        this.commandSet = commandSet;
 
     }
 
-    //TODO: Crea i set di keyword e classi che devono essere istanziate, crea un controllo se la List è vuota per capire se è un command o un parametrizedCommand
     public Command createCommandFromString(String input) {
         input = input.toLowerCase();
         Map<String,List<String>> command = commandMaker(input);
-
+        Command effectiveCommand = ;
+        if (command.get(getKeyWord(input)).isEmpty()) {
+            effectiveCommand = context.getBean(Command.class);
+        } else {
+            effectiveCommand = context.getBean(ParametrizedCommand.class);
+        }
+        return effectiveCommand;
     }
 
     public Map<String, List<String>> commandMaker(String input) {
@@ -39,5 +55,10 @@ public class CommandFactory {
         Map<String, List<String>> command = new HashMap<>();
         command.put(commandName, parameters);
         return command;
+    }
+
+    public String getKeyWord(String input) {
+        List<String> tokens = List.of(input.split(" "));
+        return tokens.get(0);
     }
 }
