dbff1beb6e0d3364bd9b198b9a2ff7d8b4afac80
Tasnim Chiara Uddin <chiaratasnim@live.it>
2023-05-31 12:19:20 +0200

    Update salvatempoReduxReduxRedux.sh

--- salvatempoReduxReduxRedux.sh
+++ salvatempoReduxReduxRedux.sh
@@ -9,14 +9,17 @@ handle_error() {
 # Spengi il container
 echo "Sto spengendo il container"
 docker stop postgres || handle_error "docker stop postgres"
+echo "Ho spento il container"
 
 # Cancella il container
 echo "Sto rimuovendo il vecchio container"
 docker rm postgres || handle_error "docker rm postgres"
+echo "Ho rimosso il vecchio container"
 
 # Avvio del container PostgreSQL
 echo "Sto creando il nuovo container"
 docker run --name postgres -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -e POSTGRES_DB=pawtropolis -p 5432:5432 -d library/postgres || handle_error "docker run postgres"
+echo "Ho creato il nuovo container"
 
 #Delay
 echo "Attendi 2 secondi..."
@@ -25,7 +28,7 @@ sleep 2
 # Copia dello script.sql nella cartella di lavoro corrente nel container PostgreSQL
 echo "Sto copiando lo script sql nel container"
 docker cp script.sql postgres:/script.sql || handle_error "docker cp script.sql"
-echo "Ho copianto lo script sql nel container"
+echo "Ho copiato lo script sql nel container"
 
 #Delay
 echo "Attendi 5 secondi..."
