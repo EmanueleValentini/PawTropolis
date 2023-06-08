06be5fcebcc3565aaad0837ee85c2b690b8db928
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-06-08 14:51:34 +0200

    Refactor Bag using Dto

--- src/main/java/it/alten/game/service/BagServiceImpl.java
+++ src/main/java/it/alten/game/service/BagServiceImpl.java
@@ -1,7 +1,9 @@
 package it.alten.game.service;
 
 import it.alten.game.model.Bag;
+import it.alten.game.model.dto.BagDto;
 import it.alten.game.repository.BagRepository;
+import it.alten.game.utils.mapper.BagMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,21 +16,27 @@ public class BagServiceImpl implements BagService{
 
     private final BagRepository bagRepository;
 
+    private final BagMapper bagMapper;
+
     @Autowired
-    public BagServiceImpl(BagRepository bagRepository) {
+    public BagServiceImpl(BagRepository bagRepository, BagMapper bagMapper) {
         this.bagRepository = bagRepository;
+        this.bagMapper = bagMapper;
     }
 
     @Override
-    public Bag save(Bag bag) {
-
-        return bagRepository.save(bag);
+    public void save(BagDto bagDto) {
+        Bag bag = bagMapper.toEntity(bagDto);
+        bagRepository.save(bag);
     }
 
     @Override
-    public Bag findById(int id) {
+    public BagDto findById(int id) {
         Optional<Bag> opt = bagRepository.findById(id);
-
-        return opt.orElse(null);
+        if (opt.isPresent()) {
+            Bag bagPresent = opt.get();
+            return bagMapper.toDTO(bagPresent);
+        }
+        return null;
     }
 }
