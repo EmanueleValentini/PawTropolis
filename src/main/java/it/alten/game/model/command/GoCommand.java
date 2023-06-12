0c30cf89333d636340dfedec83d0653129b7bded
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-06-12 17:56:03 +0200

    Refactor to add Doors

--- src/main/java/it/alten/game/model/command/GoCommand.java
+++ src/main/java/it/alten/game/model/command/GoCommand.java
@@ -1,6 +1,8 @@
 package it.alten.game.model.command;
 
 import it.alten.game.controller.GameController;
+import it.alten.game.model.Bag;
+import it.alten.game.model.Door;
 import it.alten.game.model.enums.Direction;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Scanner;
 
 @Getter
 @Setter
@@ -28,10 +31,40 @@ public class GoCommand extends ParametrizedCommand {
     @Override
     public void execute() {
         Direction direction = Direction.of(String.join(" ", parameters));
-        if (gameController.changeRoom(direction)) {
-            System.out.println("Stai in " + gameController.getCurrentRoom().roomDescription());
+        Door door = gameController.getCurrentRoom().getDoors().get(direction);
+        if (!door.isClosed()) {
+            isRoomChanged(direction);
         } else {
-            System.out.println("Nun ce poi annà");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Te serve na roba pè aprì la porta, ce voi prova? DAJE/CORCAZZO: ");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("daje")) {
+                System.out.println("Dimme qual è l'oggetto che apre sta cazzo de porta: ");
+                String itemInput = scanner.nextLine();
+                Bag bag = gameController.getPlayer().getBag();
+                if (itemInput.equals(door.getItemToOpen().getName()) && bag.getInventory().contains(door.getItemToOpen())) {
+                    door.setClosed(false);
+                    bag.getInventory().remove(door.getItemToOpen());
+                    bag.setAvailableSlots(bag.getAvailableSlots() + door.getItemToOpen().getRequestedSlots());
+                    System.out.println("Che bucio de culo era quello giusto!");
+                    isRoomChanged(direction);
+                } else {
+                    System.out.println("C'hai provato ma nun era quello giusto!");
+                }
+            } else {
+                System.out.println("E che cazzo me l'hai chiesto a fa?");
+            }
         }
+    }
+
+    public void isRoomChanged(Direction direction) {
+        boolean isRoomChanged = getGameController().getRoomController().changeRoom(direction);
+        if (isRoomChanged) {
+            System.out.println("Stai in: " + gameController.getCurrentRoom().getName());
+            System.out.println(getGameController().getRoomController().roomDescription());
+        } else {
+            System.out.println("Do la vedi la porta?");
+        }
+
     }
 }
