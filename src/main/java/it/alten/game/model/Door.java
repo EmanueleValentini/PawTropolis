69a55d7291f4286ece29156f609d272ffa8f3d88
Emanuele Valentini <emanuele_valentini@hotmail.it>
2023-06-12 17:54:59 +0200

    Add Door class

--- src/main/java/it/alten/game/model/Door.java
+++ src/main/java/it/alten/game/model/Door.java
@@ -1,2 +1,22 @@
-package it.alten.game.model;public class Door {
+package it.alten.game.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Door {
+
+    private boolean isClosed;
+
+    private Item itemToOpen;
+
+    public Door(Item itemToOpen) {
+        this.itemToOpen = itemToOpen;
+        isClosed = true;
+    }
+
+    public Door(){
+        isClosed = false;
+    }
 }
